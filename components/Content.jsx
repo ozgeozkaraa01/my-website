e1210f55c194eb8775877f6a731a578c0be4960f
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-01-17 13:00:31 +0300

    update content

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 const Content = () => {
   return (
-    <div className="flex-grow bg-darkblue-900">
-      <div className="mt-36 flex flex-col items-center md:space-y-4">
+    <div className=" flex flex-grow bg-darkblue-900">
+      <div className="mt-60 ml-40 flex flex-col items-center md:space-y-4">
         <h2 className="text-base font-medium tracking-widest uppercase text-gray-400 leading-normal">
           Frontend Developer
         </h2>
@@ -11,6 +12,14 @@ const Content = () => {
           Özgenur Özkara
         </h1>
       </div>
+      <div className="w-96 h-96 ml-72 mt-36 rounded-full border-8 border-gray-500 border-opacity-10 absolute" />
+      <figure className="max-w-sm ml-80 mt-36">
+        <img
+          src="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
+          alt="Özgenur Özkara"
+          className="object-cover rounded-[25%]"
+        ></img>
+      </figure>
     </div>
   );
 };
--- components/Header.jsx
+++ components/Header.jsx
@@ -9,12 +9,12 @@ const Header = () => {
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
         {/* Left */}
         <div className=" h-24 w-24 space-x-28 relative">
-          <Image
+          {/* <Image
             src="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
             alt="Özgenur Özkara"
             fill
             className="max-w-24 max-h-24 rounded-3xl object-cover cursor-pointer"
-          />
+          /> */}
           <div className="w-20 flex justify-center mt-8">
             <span className="font-bold text-white">Özgenur</span>
           </div>
@@ -35,7 +35,7 @@ const Header = () => {
           </a>
           <a
             className="inline-flex items-center space-x-2 transition text-gray-400 hover:text-white"
-            href="#"
+            href="https://medium.com/@ozgenurozkara" target="_blank"
           >
             <span className="whitespace-nowrap">Blog</span>
           </a>
